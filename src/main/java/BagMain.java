81f868e5537359132c9df8e66554a55f4f17cce3
yuanshu777 <yuanshu.wang@mail.utoronto.ca>
2022-09-24 23:54:10 -0400

    yuanshu wang 3

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -16,18 +16,42 @@ class BagMain {
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
         // TODO: Implement this.
-    }
+        if (double_enhance_handbags) {
+            for (Bag bag : bags) {
+                if (bag instanceof HandBag) {
+                    bag.enhance();
+                    bag.enhance();
+                } else {
+                    bag.enhance();
+                }
+            }
 
-    /**
-     * TODO: Implement this method
-     * Return the total number of straps in CrossbodyBags in bags.
-     * Any bag that is not a CrossbodyBag should not be included
-     * in the count!
-     *
-     * @param bags An array of Bags
-     * @return The total number of straps of CrossbodyBags.
-     */
-    public static int countCrossbodyStraps(Bag[] bags) {
-        // TODO: Implement this.
-    }
+        }else {
+            for (Bag bag : bags) {
+                bag.enhance();
+            }
+        }
+
+}
+
+        /**
+         * TODO: Implement this method
+         * Return the total number of straps in CrossbodyBags in bags.
+         * Any bag that is not a CrossbodyBag should not be included
+         * in the count!
+         *
+         * @param bags An array of Bags
+         * @return The total number of straps of CrossbodyBags.
+         */
+        public static int countCrossbodyStraps(Bag[] bags) {
+
+            // TODO: Implement this.
+            int totalStraps = 0;
+            for (Bag bag : bags) {
+                if (bag instanceof CrossbodyBag) {
+                    totalStraps += ((CrossbodyBag) bag).getNumberOfStraps();
+                }
+            }
+            return totalStraps;
+        }
 }
--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -30,3 +30,28 @@
  *
  *       See the tests in test/CrossBodyBagTest.java for examples.
  */
+public class CrossbodyBag extends Bag{
+
+    int numberOfStraps;
+    public CrossbodyBag(String color, int capacity, int straps) {
+        super(color, capacity);
+        this.numberOfStraps = straps;
+
+    }
+    public int getNumberOfStraps(){
+        return this.numberOfStraps;
+    }
+
+    @Override
+    public void enhance() {
+        super.increaseCapacity(2);
+    }
+
+    public String toString(){
+        return this.color + " Crossbody Bag with " + this.numberOfStraps + " straps" + " ("+ this.numberOfContents + " / " +
+                this.capacity + ")";
+
+    }
+
+
+}
